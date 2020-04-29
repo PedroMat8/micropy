f987ad97003dbbeed5442c6506fd98217f25e635
PedroMat8 <56837028+PedroMat8@users.noreply.github.com>
2020-04-29 12:20:21 +0100

    Revert "Revert "get function""
    This reverts commit d2e6896bb21dac1f36b9e3fd7e560bbe35046b89.

--- example - from given cpd.py
+++ example - from given cpd.py
@@ -13,34 +13,39 @@
     @author: PedroMat8
     @email: matteo.pedrotti@strath.ac.uk '''
 
+# =============================================================================
 # Required python packages:
-# - numpy       -->     pip install numpy
-# - matplotlib  -->     pip install matplotlib
-# - sys         -->     pip install os-sys
-# - numba       -->     pip install numba
-
+# - numpy       -->     conda install numpy
+# - matplotlib  -->     conda install matplotlib
+# - numba       -->     conda install numba
+#               -->     conda install -n micropy cudatoolkit
+#
 # Keep the input data in a subfolder named "input".
 # Input files columns are pore diameter[um] and cumulative void ratio[-].
 # No headers.
-##############################################################################
-##############################################################################
+# =============================================================================
+
 
 from micropy import pore_distribution as pore
 
-# Input parameters
+# =============================================================================
+# # Input parameters
+# =============================================================================
 intervals = 35          # Number of intervals for frequency
 dmax = 770              # [um] --> Maximum diameter
 dmin = 0.0044           # [um] --> Minimum diameter
 
-# There should no need to touch anything below this line
+# =============================================================================
+# # There should no need to touch anything below this line
+# =============================================================================
 inputs = {
         'intervals': intervals,
         'dmax': dmax, 'dmin': dmin
         }
 
 data = pore.DataElaboration(inputs)
-data.cpd_from_file('input\input-edo2.txt')
-[data.psd.d, data.psd.e] = data.psd.psd_from_cpd(data.cpd.d, data.cpd.e)
+data.get_cpd_from_file('input\input-edo2.txt')
+[data.psd.d, data.psd.e] = data.psd.get_psd_from_cpd(data.cpd.d, data.cpd.e)
 # data.norm_dist()
 
 # Plot cpd and psd
